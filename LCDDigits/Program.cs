ab1ee998e7e5dd3ced038adee0c964ea4b4adbd6
Oleksii Yurchenko <Oleksii_Yurchenko@epam.com>
2021-03-30 23:17:33 +0300

    [Task 2.1] Add program implementation

--- LCDDigits/Program.cs
+++ LCDDigits/Program.cs
@@ -1,9 +1,14 @@
-﻿namespace LCDDigits
+﻿using System;
+
+namespace LCDDigits
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Input your number to its LCD representation");
+            var inputNumber = int.Parse(Console.ReadLine() ?? string.Empty);
+            Console.WriteLine(inputNumber.GetLcdStringNumber());
         }
     }
 }
