d44cd6c9b01ddc9bfcc004cb7ffe5b1efb021219
Oleksii Yurchenko <Oleksii_Yurchenko@epam.com>
2021-03-31 23:57:12 +0300

    [Task 2.3] Add IsLeapYear_GivenLeapYear_ReturnsTrue

--- LeapYear.Tests/Tests.cs
+++ LeapYear.Tests/Tests.cs
@@ -5,6 +5,18 @@ namespace LeapYear.Tests
 {
     public class Tests
     {
-       
+        [Fact]
+        public void IsLeapYear_GivenLeapYear_ReturnsTrue()
+        {
+            // Arrange :D
+            const int givenYear = 1984;
+            const bool expectedFlag = true;
+
+            // Act
+            var result = LeapYear.IsLeapYear(givenYear);
+
+            // Assert
+            Assert.Equal(expectedFlag, result);
+        }
     }
 }
---
+++ LeapYear/LeapYear.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LeapYear
+{
+    public static class LeapYear
+    {
+        public static bool IsLeapYear(int year)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
