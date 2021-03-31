eb0be6ee2f4fc793f65c8e8d3198650e56662150
Oleksii Yurchenko <Oleksii_Yurchenko@epam.com>
2021-04-01 00:31:26 +0300

    [Task 2.3] Add IsLeapYear usage into the program

--- LeapYear/Program.cs
+++ LeapYear/Program.cs
@@ -6,7 +6,9 @@ namespace LeapYear
     {
         static void Main(string[] args)
         {
-            
+            Console.WriteLine("Enter the year: ");
+            var year = int.Parse(Console.ReadLine() ?? string.Empty);
+            Console.WriteLine($"Is year leap: {LeapYear.IsLeapYear(year)}");
         }
     }
 }
