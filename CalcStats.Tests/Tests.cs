32f6987d2a16984af42cc0826584304ffd21d2b1
Oleksii Yurchenko <Oleksii_Yurchenko@epam.com>
2021-03-31 21:55:50 +0300

    [Task 2.2] Add GetStatistic_GivenOneNumber_ReturnsMaximumValue

--- CalcStats.Tests/Tests.cs
+++ CalcStats.Tests/Tests.cs
@@ -18,5 +18,19 @@ namespace CalcStats.Tests
             // Assert
             Assert.Equal(expectedMinimum, result);
         }
+        
+        [Fact]
+        public void GetStatistic_GivenOneNumber_ReturnsMaximumValue()
+        {
+            // Arrange
+            const int number = 1;
+            const int expectedMaximum = 1;
+
+            // Act
+            var result = CalcStatistic.GetStatistic(number);
+
+            // Assert
+            Assert.Equal(expectedMaximum, result);
+        }
     }
 }
