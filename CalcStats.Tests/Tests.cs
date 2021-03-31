35c8c273aaecb8df29529934b2cf3efb3680e015
Oleksii Yurchenko <Oleksii_Yurchenko@epam.com>
2021-03-31 21:50:33 +0300

    [Task 2.2] Add GetStatistic_GivenOneNumber_ReturnsMinimumValue

--- CalcStats.Tests/Tests.cs
+++ CalcStats.Tests/Tests.cs
@@ -5,5 +5,18 @@ namespace CalcStats.Tests
 {
     public class Tests
     {
+        [Fact]
+        public void GetStatistic_GivenOneNumber_ReturnsMinimumValue()
+        {
+            // Arrange
+            const int number = 1;
+            const int expectedMinimum = 1;
+
+            // Act
+            var result = CalcStatistic.GetStatistic(number);
+
+            // Assert
+            Assert.Equal(expectedMinimum, result);
+        }
     }
 }
---
+++ CalcStats/CalcStatistic.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CalcStats
+{
+    public static class CalcStatistic
+    {
+        public static double GetStatistic(int number)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
