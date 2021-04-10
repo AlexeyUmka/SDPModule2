b8d1cc56053cfbe2e2736f79420d7a583554c5e5
KYIV\Oleksii_Yurchenko <Oleksii_Yurchenko@epam.com>
2021-04-10 20:09:30 +0300

    [Task 3.1] Refactor Container.cs

--- Task1/Container.cs
+++ Task1/Container.cs
@@ -17,21 +17,16 @@ namespace Task1
             foreach (var type in types)
             {
                 var exportAttribute = type.GetCustomAttribute<ExportAttribute>();
+                var importAttribute = type.GetCustomAttribute<ImportConstructorAttribute>();
                 var importProperties = type.GetProperties().Where(prop => prop.GetCustomAttribute<ImportAttribute>() != null);
-                if (type.GetCustomAttribute<ImportConstructorAttribute>()!=null)
+                if (importAttribute != null)
                 {
                     _typeImplementationDependencies.Add(type, type);
                 }
                 else if (exportAttribute != null)
                 {
-                    if (exportAttribute.Contract == null)
-                    {
-                        _typeImplementationDependencies.Add(type, type);
-                    }
-                    else
-                    {
-                        _typeImplementationDependencies.Add(exportAttribute.Contract, type);
-                    }
+                    _typeImplementationDependencies.Add(
+                        exportAttribute.Contract == null ? type : exportAttribute.Contract, type);
                 }
                 else if(importProperties.Any())
                 {
@@ -55,61 +50,67 @@ namespace Task1
             var implementationType =
                 _typeImplementationDependencies.LastOrDefault(serviceDescriptor => serviceDescriptor.Key == typeof(T) || serviceDescriptor.Value == typeof(T));
             var constructorParameters = implementationType.Value.GetConstructors()[0].GetParameters();
+            var importedProperties = implementationType.Value.GetProperties()
+                .Where(prop => prop.GetCustomAttribute<ImportAttribute>() != null).ToList();
             if (constructorParameters.Length > 0)
             {
-                var exportAttribute = implementationType.Value.GetCustomAttribute<ExportAttribute>();
-                var importConstructorAttribute =
-                    implementationType.Key.GetCustomAttribute<ImportConstructorAttribute>();
-                if (exportAttribute != null)
-                {
-                    if (exportAttribute.Contract == null)
-                    {
-                        _typeImplementationDependencies.Add(implementationType.Value, implementationType.Value);
-                    }
-                    else
-                    {
-                        _typeImplementationDependencies.Add(exportAttribute.Contract, implementationType.Value);
-                    }
-
-                    var method = typeof(Container).GetMethod(nameof(Get));
-                    var generic = method.MakeGenericMethod(implementationType.Key);
-                    return (T) generic.Invoke(this, null);
-                }
-
-                if (importConstructorAttribute != null)
-                {
-                    var parameters = new List<object>();
-                    foreach (var parameter in constructorParameters)
-                    {
-                        object implementationParameter = null;
-                        var parameterType = parameter.ParameterType;
-                        var method = typeof(Container).GetMethod(nameof(Get));
-                        var generic = method.MakeGenericMethod(parameterType);
-                        implementationParameter = generic.Invoke(this, null);
-                        parameters.Add(implementationParameter);
-                    }
-
-                    return (T) Activator.CreateInstance(implementationType.Value, parameters.ToArray());
-                }
+                return GetImplementationForTypeWithConstructor<T>(implementationType, constructorParameters);
             }
-            else if (implementationType.Value.GetProperties()
-                .Any(p => p.GetCustomAttribute<ImportAttribute>() != null))
+            if (importedProperties.Any())
             {
-                var implementation = Activator.CreateInstance(implementationType.Value);
-                foreach (var property in implementationType.Value.GetProperties().Where(prop => prop.GetCustomAttribute<ImportAttribute>() != null))
-                {
-                    var parameterType = property.PropertyType;
-                    var method = typeof(Container).GetMethod(nameof(Get));
-                    var generic = method.MakeGenericMethod(parameterType);
-                    object propertyImplementation = null;
-                    propertyImplementation = generic.Invoke(this, null);
-                    property.SetValue(implementation, propertyImplementation);
-                }
-
-                return (T) implementation;
+                return GetImplementationForTypeWithImportedProperties<T>(implementationType, importedProperties);
             }
 
             return (T) Activator.CreateInstance(implementationType.Value);
+        }
+
+        private T GetImplementationForTypeWithImportedProperties<T>(KeyValuePair<Type, Type> implementationType, IEnumerable<PropertyInfo> propertyInfos)
+        {
+            var implementation = Activator.CreateInstance(implementationType.Value);
+            foreach (var property in implementationType.Value.GetProperties().Where(prop => prop.GetCustomAttribute<ImportAttribute>() != null))
+            {
+                var parameterType = property.PropertyType;
+                var method = typeof(Container).GetMethod(nameof(Get));
+                var generic = method.MakeGenericMethod(parameterType);
+                object propertyImplementation = null;
+                propertyImplementation = generic.Invoke(this, null);
+                property.SetValue(implementation, propertyImplementation);
+            }
+
+            return (T) implementation;
+        }
+
+        private T GetImplementationForTypeWithConstructor<T>(KeyValuePair<Type, Type> implementationType, ParameterInfo[] constructorParameters)
+        {
+            var exportAttribute = implementationType.Value.GetCustomAttribute<ExportAttribute>();
+            var importConstructorAttribute =
+                implementationType.Key.GetCustomAttribute<ImportConstructorAttribute>();
+            if (exportAttribute != null)
+            {
+                _typeImplementationDependencies.Add(
+                    exportAttribute.Contract == null ? implementationType.Value : exportAttribute.Contract,
+                    implementationType.Value);
+
+                var method = typeof(Container).GetMethod(nameof(Get));
+                var generic = method.MakeGenericMethod(implementationType.Key);
+                return (T) generic.Invoke(this, null);
+            }
+
+            if (importConstructorAttribute == null) return (T) Activator.CreateInstance(implementationType.Value);
+            {
+                var parameters = new List<object>();
+                foreach (var parameter in constructorParameters)
+                {
+                    object implementationParameter = null;
+                    var parameterType = parameter.ParameterType;
+                    var method = typeof(Container).GetMethod(nameof(Get));
+                    var generic = method.MakeGenericMethod(parameterType);
+                    implementationParameter = generic.Invoke(this, null);
+                    parameters.Add(implementationParameter);
+                }
+
+                return (T) Activator.CreateInstance(implementationType.Value, parameters.ToArray());
+            }
         }
     }
 }
